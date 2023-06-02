44d5bb26ccade64cdcf68593759204732d76cae6
eliasmaq <eliasmesones@gmail.com>
2023-06-02 17:33:32 -0500

    Added more routes and put method

--- server.js
+++ server.js
@@ -1,39 +1,66 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
-const products = [
-    {
-        id: 1,
-        name: "laptop",
-        price: 3000
-    }
-]
+const app = express();
+let products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 3000,
+  },
+];
 
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.get('/products', (req, res) => {
-    res.send('vista de productos')
-})
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
-app.post('/products', (req, res) => {
-    console.log({...req.body, id: products.length + 1 })
-    products.push(req.body)
-    res.send('creando productos')
-})
+app.post("/products", (req, res) => {
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products.push(newProduct);
+  res.send(newProduct);
+});
 
-app.put('/products', (req, res) => {
-    res.send('actualizando productos')
-})
+app.put("/products/:id", (req, res) => {
 
-app.delete('/products', (req, res) => {
-    res.send('eliminando productos')
-})
+    const newData = req.body
+    const productFound = products.find((p) => p.id === parseInt(req.params.id));
 
-app.get('/products/:id', (req, res) => {
-    res.send('obteniendo un producto')
-})
+    if (!productFound)
+      return res.status(404).json({
+        message: "product not found",
+      });
+    
+    const newProducts = products.map(p => p.id === parseInt(req.params.id) ? {...p, ...newData} : p)
+    console.log(newProducts)
+    res.send("actualizando productos");
+});
 
-app.listen(3000)
-console.log('server on port 3000')
+app.delete("/products/:id", (req, res) => {
+  const productFound = products.find((p) => p.id === parseInt(req.params.id));
+
+  if (!productFound)
+    return res.status(404).json({
+      message: "product not found",
+    });
+  products = products.filter((p) => p.id !== parseInt(req.params.id));
+
+  res.sendStatus(204);
+});
+
+app.get("/products/:id", (req, res) => {
+  const productFound = products.find((p) => p.id === parseInt(req.params.id));
+
+  if (!productFound)
+    return res.status(404).json({
+      message: "product not found",
+    });
+
+  res.json(productFound);
+  console.log(productFound);
+});
+
+app.listen(3000);
+console.log("server on port 3000");
